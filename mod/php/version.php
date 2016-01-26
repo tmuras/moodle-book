04b97c761216eaac2174fcc40a82cda201a40d0b
Tomasz Muras <nexor1984@gmail.com>
2016-01-26 07:30:29 +0000

    Version bump, Moodle required added, optional param removed.

--- mod/php/version.php
+++ mod/php/version.php
@@ -24,5 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015110900; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2016013000; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2015111000;    // Requires this Moodle version
 $plugin->component = 'mod_php'; // Full name of the plugin (used for diagnostics)
