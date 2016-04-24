40509b88c1d6364793a03f49125887953e2900b6
Tomasz Muras <nexor1984@gmail.com>
2016-04-24 14:56:02 +0100

    WIP

--- mod/php/locallib.php
+++ mod/php/locallib.php
@@ -3,18 +3,22 @@
  * Persist $submission in DB.
  */
 function mod_php_save_submission($submission) {
-    global $DB;
-    // $DB->insert_record('')
+    global $DB, $USER;
+
+    $submission->userid = $USER->id;
+    $submission->timecreated = time();
+
+    return $DB->insert_record('php_submissions',$submission);
 }
 
 /**
  * Get PHP submission for the current user (if exists).
  * @return stdClass
  */
-function mod_php_get_my_submission() {
+function mod_php_get_my_submission($phpid) {
     global $DB, $USER;
 
-    $submission = $DB->get_records('php_submissions',array());
+    $submission = $DB->get_record('php_submissions', array('phpid' => $phpid, 'userid' => $USER->id));
 
     return $submission;
 }
